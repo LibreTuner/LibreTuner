01d112b00f753728d431e18382f905a8d64f405a
Altenius <jacobjm18@gmail.com>
2018-08-22 21:33:45 -0400

    Started moving diagnostics to widget

---
+++ src/ui/diagnosticswidget.cpp
@@ -0,0 +1,6 @@
+#include "diagnosticswidget.h"
+
+DiagnosticsWidget::DiagnosticsWidget(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ src/ui/diagnosticswidget.h
@@ -0,0 +1,17 @@
+#ifndef DIAGNOSTICSWIDGET_H
+#define DIAGNOSTICSWIDGET_H
+
+#include <QWidget>
+
+class DiagnosticsWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit DiagnosticsWidget(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // DIAGNOSTICSWIDGET_H
