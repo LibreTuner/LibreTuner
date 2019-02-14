2c8b53bb019b28ad8f932d64240af3e55fb3705e
Altenius <jacobjm18@gmail.com>
2019-02-14 13:00:54 -0500

    Added overview dock

---
+++ src/ui/docks/overviewwidget.cpp
@@ -0,0 +1,35 @@
+#include "overviewwidget.h"
+
+#include "rommanager.h"
+#include "definitions/definitionmanager.h"
+#include "libretuner.h"
+
+#include <QVBoxLayout>
+#include <QFormLayout>
+#include <QLineEdit>
+
+OverviewWidget::OverviewWidget(QWidget* parent) : QWidget(parent)
+{
+    lineDefinitionCount_ = new QLineEdit;
+    lineDefinitionCount_->setReadOnly(true);
+    
+    lineRomCount_ = new QLineEdit;
+    lineRomCount_->setReadOnly(true);
+    
+    lineDatalinkCount_ = new QLineEdit;
+    lineDatalinkCount_->setReadOnly(true);
+    
+    QFormLayout *form = new QFormLayout;
+    form->addRow(tr("Definitions loaded:"), lineDefinitionCount_);
+    form->addRow(tr("ROMs loaded:"), lineRomCount_);
+    form->addRow(tr("Datalinks detected:"), lineDatalinkCount_);
+    
+    lineRomCount_->setText(QString::number(RomStore::get()->count()));
+    lineDefinitionCount_->setText(QString::number(DefinitionManager::get()->rowCount(QModelIndex())));
+    lineDatalinkCount_->setText(QString::number(LT()->datalinks().rowCount(QModelIndex())));
+    
+    auto *layout = new QVBoxLayout;
+    layout->addLayout(form);
+    
+    setLayout(layout);
+}
---
+++ src/ui/docks/overviewwidget.h
@@ -0,0 +1,23 @@
+#ifndef OVERVIEWWIDGET_H
+#define OVERVIEWWIDGET_H
+
+#include <QWidget>
+
+class QLineEdit;
+
+/**
+ * Shows an overview
+ */
+class OverviewWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit OverviewWidget(QWidget *parent = nullptr);
+    
+private:
+    QLineEdit *lineDefinitionCount_;
+    QLineEdit *lineRomCount_;
+    QLineEdit *lineDatalinkCount_;
+};
+
+#endif // OVERVIEWWIDGET_H
