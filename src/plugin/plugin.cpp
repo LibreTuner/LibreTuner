c116e3a288157dfd731e3ce2c3a8b82ee659f806
Altenius <jacobjm18@gmail.com>
2019-02-24 20:35:52 -0500

    Started plugin system

---
+++ src/plugin/plugin.cpp
@@ -0,0 +1,8 @@
+#include "plugin.h"
+
+#include <fstream>
+
+Plugin::Plugin(PluginState& state, const std::string& path) : state_(&state)
+{
+    
+}
---
+++ src/plugin/pluginstate.cpp
@@ -0,0 +1,21 @@
+#include "pluginstate.h"
+
+#include "logger.h"
+
+
+PluginState::PluginState()
+{
+    // Open libraries
+    lua_.open_libraries(sol::lib::base, sol::lib::package, sol::lib::coroutine, sol::lib::string, sol::lib::os, sol::lib::math, sol::lib::table, sol::lib::debug, sol::lib::bit32, sol::lib::io);
+}
+
+
+
+void PluginState::loadFile(const std::string& filename)
+{
+    try {
+        lua_.safe_script_file(filename);
+    } catch (const sol::error &err) {
+        Logger::warning("Error loading " + filename + ": " + err.what());
+    }
+}
