0a8c7859371da6604a0f46127faab8d9e3df0db5
Altenius <jacobjm18@gmail.com>
2019-04-18 12:39:32 -0400

    Started hex edit widget

--- src/ui/docks/hexeditwidget.cpp
+++ src/ui/docks/hexeditwidget.cpp
@@ -1,5 +1,57 @@
-//
-// Created by altenius on 4/18/19.
-//
-
 #include "hexeditwidget.h"
+
+#include <QHBoxLayout>
+
+HexEditWidget::HexEditWidget(QWidget *parent) : QWidget(parent) {
+    buffer_ = new RomHexBuffer;
+
+    auto *document = new QHexDocument(buffer_);
+    auto *hexView = new QHexView;
+    hexView->setDocument(document);
+
+    auto *layout = new QHBoxLayout;
+    layout->setContentsMargins(0, 0, 0, 0);
+
+    layout->addWidget(hexView);
+
+    setLayout(layout);
+}
+
+int RomHexBuffer::length() const {
+    if (!rom_) {
+        return 0;
+    }
+
+    return rom_->size();
+}
+
+void RomHexBuffer::insert(int offset, const QByteArray &data) {
+
+}
+
+void RomHexBuffer::remove(int offset, int length) {
+
+}
+
+QByteArray RomHexBuffer::read(int offset, int length) {
+    if (!rom_) {
+        return QByteArray();
+    }
+    if (offset >= rom_->size()) {
+        return QByteArray();
+    }
+    int size = std::min<int>(rom_->size() - offset, length);
+
+    return QByteArray(reinterpret_cast<const char*>(&rom_->data()[offset]), size);
+}
+
+void RomHexBuffer::read(QIODevice *iodevice) {
+
+}
+
+void RomHexBuffer::write(QIODevice *iodevice) {
+    if (!rom_) {
+        return;
+    }
+    iodevice->write(reinterpret_cast<const char*>(rom_->data()), rom_->size());
+}
--- src/ui/docks/hexeditwidget.h
+++ src/ui/docks/hexeditwidget.h
@@ -1,13 +1,38 @@
-//
-// Created by altenius on 4/18/19.
-//
-
 #ifndef LIBRETUNER_HEXEDITWIDGET_H
 #define LIBRETUNER_HEXEDITWIDGET_H
 
+#include <QWidget>
+#include <qhexview.h>
 
-class HexEditWidget {
+#include "lt/rom/rom.h"
 
+class RomHexBuffer : public QHexBuffer {
+    Q_OBJECT
+public:
+    explicit RomHexBuffer(lt::RomPtr rom = lt::RomPtr()) : rom_(std::move(rom)) {}
+
+    int length() const override;
+    void insert(int offset, const QByteArray &data) override;
+    void remove(int offset, int length) override;
+    QByteArray read(int offset, int length) override;
+    void read(QIODevice *iodevice) override;
+    void write(QIODevice *iodevice) override;
+
+    inline void setRom(lt::RomPtr rom) noexcept { rom_ = std::move(rom); }
+
+private:
+    lt::RomPtr rom_;
+};
+
+class HexEditWidget : public QWidget {
+    Q_OBJECT
+public:
+    explicit HexEditWidget(QWidget *parent = nullptr);
+
+    inline void setRom(lt::RomPtr rom) noexcept { buffer_->setRom(std::move(rom)); }
+
+private:
+    RomHexBuffer *buffer_;
 };
 
 
