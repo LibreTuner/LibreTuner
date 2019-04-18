1a1599fb295bede3c96a47b3207c20d75576f684
Altenius <jacobjm18@gmail.com>
2019-04-18 11:57:52 -0400

    Updated QHexView

---
+++ src/ui/docks/hexeditwidget.cpp
@@ -0,0 +1,5 @@
+//
+// Created by altenius on 4/18/19.
+//
+
+#include "hexeditwidget.h"
---
+++ src/ui/docks/hexeditwidget.h
@@ -0,0 +1,14 @@
+//
+// Created by altenius on 4/18/19.
+//
+
+#ifndef LIBRETUNER_HEXEDITWIDGET_H
+#define LIBRETUNER_HEXEDITWIDGET_H
+
+
+class HexEditWidget {
+
+};
+
+
+#endif //LIBRETUNER_HEXEDITWIDGET_H
