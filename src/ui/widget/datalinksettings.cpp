b2036e5360d8ae37696acd37cb27bd6f58fc332e
Jacob Manning <jacobjm18@gmail.com>
2019-05-24 13:54:49 -0400

    Started datalink settings widget

---
+++ src/ui/widget/datalinksettings.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jacob on 5/24/2019.
+//
+
+#include "datalinksettings.h"
---
+++ src/ui/widget/datalinksettings.h
@@ -0,0 +1,16 @@
+//
+// Created by Jacob on 5/24/2019.
+//
+
+#ifndef LIBRETUNER_DATALINKSETTINGS_H
+#define LIBRETUNER_DATALINKSETTINGS_H
+
+
+
+class DataLinkSettings {
+
+};
+
+
+
+#endif //LIBRETUNER_DATALINKSETTINGS_H
